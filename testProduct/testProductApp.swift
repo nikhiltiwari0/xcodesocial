30a416ab88f0b6101fa57baaccf058af29d5cea0
Nikhil Tiwari <tiwarin9211@gmail.com>
2025-05-13 22:12:09 -0400

    Initial Commit

---
+++ testProduct/testProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProductApp.swift
+//  testProduct
+//
+//  Created by Nikhil Tiwari on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct testProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testProductTests/testProductTests.swift
@@ -0,0 +1,16 @@
+//
+//  testProductTests.swift
+//  testProductTests
+//
+//  Created by Nikhil Tiwari on 5/13/25.
+//
+
+import Testing
+
+struct testProductTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
